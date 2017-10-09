9c451d8c2e750ae2736d6a5cb30ed5492b0fa097
Weekend Master <weekend@super.com>
2017-10-09 21:04:48 +0300

    created test for Graph

---
+++ test/test_Graph/test_Graph.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <gtest/gtest.h>
+
+#include "../../src/WordEngine/Graph.h"
+
+int main(int argc, char** argv) {
+
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
